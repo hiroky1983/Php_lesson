ebaba1fde982f742f07dca4bd2b48cc1f18cf7bd
yamadahiroki <hirockysan1983@gmail.com>
2022-05-13 21:46:00 +0900

    パッケージ更新

--- api/routes/api.php
+++ api/routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/hello', function () {
-    return 'Hello Next.js, from Laravel API';
+    return 'Hello Next.js, from Laravel API Now';
 });
