c56d64225754f29602d020eadba459c3c20ce087
yamadahiroki <hirockysan1983@gmail.com>
2022-05-16 16:01:41 +0900

    PHPのデータを配列に変更

--- api/routes/api.php
+++ api/routes/api.php
@@ -15,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/hello', function () {
-    return 'Hello Next.js, from Laravel API Now';
+    return [
+        ['name' => 'John' ],
+        ['name' => 'Jane' ],
+        ['name' => 'Bob' ],
+        ['name' => 'Hello Next.js, from Laravel API Now']
+    ];
 });
