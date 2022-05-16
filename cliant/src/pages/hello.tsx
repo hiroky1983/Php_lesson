108ca2f3dc309074faa8bde47634839f20eab8f6
yamadahiroki <hirockysan1983@gmail.com>
2022-05-16 16:01:58 +0900

    リストのスタイリング

--- cliant/src/pages/hello.tsx
+++ cliant/src/pages/hello.tsx
@@ -3,21 +3,29 @@ import type { NextPage } from "next";
 import useSWR from "swr";
 import axios from "../lib/axios";
 
+type Data = {
+  name: string;
+};
+
 const Hello: NextPage = () => {
-  const { data, error } = useSWR("/api/hello", () =>
-    axios
-      .get("/api/hello")
-      .then((res: AxiosResponse) => res.data)
+  const { data, error } = useSWR<Data[]>("/api/hello", () =>
+    axios.get("/api/hello").then((res: AxiosResponse) => res.data)
   );
   console.log(data);
 
   if (error) return <div>エラーが発生しました</div>;
   if (!data) return <div>読み込み中</div>;
   return (
-    <div>
-      <h1>ようこそ, next.js</h1>
+    <div className="m-auto w-4/5">
+      <h1 className="shadow-xl">ようこそ, next.js</h1>
       <hr />
-      <p>{data}-1234</p>
+      <ul className="mt-4 grid grid-cols-2 gap-7">
+        {data.map((d, i) => (
+          <li key={i} className="rounded-xl text-center shadow-2xl p-4">
+            {d.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
