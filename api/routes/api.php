262e513cfa207e22b91c55791f4ae41e9752bc1b
yamadahiroki <hirockysan1983@gmail.com>
2022-05-18 22:19:03 +0900

    Task一覧のAPIを実装

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource('tasks','TaskController');
+
 
 Route::get('/hello', function () {
     return [
