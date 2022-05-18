52bfbc469c97f0ae4918bdd3fc48908a20be7c07
yamadahiroki <hirockysan1983@gmail.com>
2022-05-18 17:24:13 +0900

    コミット

--- api/database/seeders/TaskSeeder.php
+++ api/database/seeders/TaskSeeder.php
@@ -4,16 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Task;
 
-class DatabaseSeeder extends Seeder
+class TaskSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-      $this->call(TaskSeeder::class);
+        Task::factory()->count(10)->create();
     }
 }
