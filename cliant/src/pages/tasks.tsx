3ab0b6df338027704827d88e52c521c768efc166
yamadahiroki <hirockysan1983@gmail.com>
2022-05-22 01:41:03 +0900

    log消し

--- cliant/src/pages/tasks.tsx
+++ cliant/src/pages/tasks.tsx
@@ -29,7 +29,6 @@ const Tasks: NextPage = () => {
   if (error) return <div>エラーが発生しました</div>;
   if (!data) return <div>読み込み中</div>;
 
-  console.log(data);
 
   return (
     <div className="mx-20">
